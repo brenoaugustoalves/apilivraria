fddad9c4f71cdbee0a63bc928a80f2f0aea8b9de
brenoaugustoalves <30992451+brenoaugustoalves@users.noreply.github.com>
2020-02-06 17:36:46 -0300

    Add files via upload

---
+++ 2020_02_06_201157_create_livros_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLivrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->string('titulo',20);
+            $table->string('autor',20);
+            $table->string('descricao',50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livros');
+    }
+}
