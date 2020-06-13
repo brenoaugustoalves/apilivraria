173f9cadf469aff9e4329b5ba9547974cfe099f2
brenoaugustoalves <30992451+brenoaugustoalves@users.noreply.github.com>
2020-06-13 17:42:36 -0300

    Update api.php

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 
-Route::post('criar-livro','LivroController@criarlivro');
-Route::delete('deletar-livro','LivroController@deletar');
-Route::put('atualizar-livro{id}', 'LivroController@atualizar');
-Route::get('selecionar-livro{id}','LivroController@selecionar');
+  Route::post('criar-livro','LivroController@criarlivro');
+  Route::delete('deletar-livro','LivroController@deletar');
+  Route::put('atualizar-livro{id}', 'LivroController@atualizar');
+  Route::get('selecionar-livro{id}','LivroController@selecionar');
